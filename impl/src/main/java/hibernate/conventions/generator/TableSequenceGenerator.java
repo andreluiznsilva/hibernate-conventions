7aa4285cd664c324b1a460ccbd1cd67790fdcd08
Andre Nascimento <andreluiznsilva@gmail.com>
2014-08-21 09:37:28 -0300

    teste TableSequenceGenerator

--- impl/src/main/java/hibernate/conventions/generator/TableSequenceGenerator.java
+++ impl/src/main/java/hibernate/conventions/generator/TableSequenceGenerator.java
@@ -7,15 +7,14 @@ import org.hibernate.dialect.Dialect;
 import org.hibernate.id.SequenceGenerator;
 import org.hibernate.type.Type;
 
-public class CustomSequenceGenerator extends SequenceGenerator {
+public class TableSequenceGenerator extends SequenceGenerator {
 
 	@Override
 	public void configure(Type type, Properties params, Dialect dialect) throws MappingException {
 		if (params.getProperty(SEQUENCE) == null || params.getProperty(SEQUENCE).length() == 0) {
 			String tableName = params.getProperty(TABLE);
 			if (tableName != null) {
-				String seqName = "SEQ_" + tableName + "";
-				seqName = "\"" + seqName.toLowerCase() + "\"";
+				String seqName = "seq_" + tableName + "";
 				params.setProperty(SEQUENCE, seqName);
 			}
 		}
