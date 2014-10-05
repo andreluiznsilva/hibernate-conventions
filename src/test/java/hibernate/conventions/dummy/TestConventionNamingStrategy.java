bd5727c9dc378ae55e2f8a3804e7be4a03201ba7
andreluiznsilva <andreluiznsilva@gmail.com>
2014-10-05 09:00:58 -0300

    configuração do ConventionNamingStrategy pelo persistence.xml

---
+++ src/test/java/hibernate/conventions/dummy/TestConventionNamingStrategy.java
@@ -0,0 +1,7 @@
+package hibernate.conventions.dummy;
+
+import hibernate.conventions.strategy.DefaultConventionNamingStrategy;
+
+public class TestConventionNamingStrategy extends DefaultConventionNamingStrategy {
+
+}
