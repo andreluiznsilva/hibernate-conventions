ddafb177a3ad728c4c031b5f4ef2c62accd8773b
andreluiznsilva <andreluiznsilva@gmail.com>
2014-10-05 10:21:40 -0300

    teste para postgre

---
+++ src/test/java/hibernate/conventions/DDLConventionsForPostgreSQL9DialectIT.java
@@ -0,0 +1,78 @@
+package hibernate.conventions;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import hibernate.conventions.util.ConventionUtils;
+
+import java.util.List;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.hibernate.cfg.Configuration;
+import org.hibernate.dialect.PostgreSQL9Dialect;
+import org.hibernate.service.ServiceRegistry;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class DDLConventionsForPostgreSQL9DialectIT {
+
+	private EntityManagerFactory entityManagerFactory;
+	private Configuration configuration;
+	private ServiceRegistry serviceRegistry;
+	private DDLConventions conventions;
+
+	@Before
+	public void setUp() throws Exception {
+		entityManagerFactory = Persistence.createEntityManagerFactory("test");
+		serviceRegistry = ConventionUtils.extractServiceRegistry(entityManagerFactory);
+		configuration = ConventionUtils.extractConfiguration(entityManagerFactory);
+		conventions = new DDLConventions(configuration, serviceRegistry, new PostgreSQL9Dialect());
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		entityManagerFactory.close();
+	}
+
+	@Test
+	public void testGenerateCleanScript() {
+		List<String> script = conventions.generateCleanScript();
+		assertSql(script,
+				"TRUNCATE DummyEntity CASCADE");
+	}
+
+	@Test
+	public void testGenerateCreateScript() {
+		List<String> script = conventions.generateCreateScript();
+		assertSql(script,
+				"create table DummyEntity (id bigint not null, name varchar(255), primary key (id))",
+				"create sequence seq_DummyEntity");
+	}
+
+	@Test
+	public void testGenerateDropScript() {
+		List<String> script = conventions.generateDropScript();
+		assertSql(script,
+				"drop table DummyEntity cascade",
+				"drop sequence seq_DummyEntity");
+	}
+
+	@Test
+	@Ignore
+	// TODO: Verificar exceção
+	public void testGenerateUpdateScript() {
+		List<String> script = conventions.generateUpdateScript();
+		assertTrue(script.isEmpty());
+	}
+
+	private void assertSql(List<String> script, String... sqls) {
+		assertEquals(sqls.length, script.size());
+		for (int i = 0; i < sqls.length; i++) {
+			assertEquals(sqls[i], script.get(i));
+		}
+	}
+
+}
